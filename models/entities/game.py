27b2b67ff37d3c0ecc5f3d9d3fadba795ea3600b
Sergio <sergionunesd@uma.es>
2019-11-20 19:32:03 +0100

    Sergio2 (#1)
    * init
    
    * fixed

---
+++ models/entities/game.py
@@ -0,0 +1,14 @@
+from google.appengine.ext import db
+from models.entities.user import User
+from models.entities.zone import Zone
+from models.entities.treasure import Treasure
+
+
+class Game(db.Model):
+    name = db.StringProperty(required=True)
+    is_active = db.BooleanProperty(required=True)
+    winner = db.ReferenceProperty(User, collection_name="won_games")
+    zone = db.ReferenceProperty(Zone, collection_name="game", required=True)
+    treasures = db.ReferenceProperty(Treasure, collection_name="game", required=True)
+    owner = db.ReferenceProperty(User, collection_name="created_games", required=True)
+    participants = db.ReferenceProperty(User, collection_name="participating_games", required=True)
