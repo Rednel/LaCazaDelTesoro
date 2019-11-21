1e1eb341edee89fd8eb7a86ac45cd72be4370b8e
Alberto Reyes <albertoreyesmartin@gmail.com>
2019-11-21 18:27:23 +0100

    CRUD y Facades de entidades treasure y snapshot

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/treasure")  # creates a new treasure
+def treasures():
+    return render_template("treasures.html")
+
+
+if __name__ == "__main__":  # on running python app.py
+    app.run(debug=True)  # run the flask app
+
+
+
