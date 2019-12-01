76a73722bb145afcc0c2c15eeb1e51fb71ef272b
Tarek Khalfaoui <khalfaoui.t@hotmail.com>
2019-12-01 13:32:06 +0100

    route for user and zona CRUD

--- main.py
+++ main.py
@@ -1,9 +1,67 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    return 'Hello World!'
+
+
+""" 
+CRUD User routes       
+"""
+
+
+# user list
+@app.route('/user_all')
+def user_all():
+    return 'Hello World!'
+
+
+#  get user info
+@app.route('/user_one')
+def user_one():
+    return 'Hello World!'
+
+
+#  update user info
+@app.route('/user_edit')
+def user_edit():
+    return 'Hello World!'
+
+
+#  delete user
+@app.route('/user_delete')
+def user_delete():
+    return 'Hello World!'
+
+
+""" 
+CRUD Zona routes       
+"""
+
+# zona list
+@app.route('/zona_all')
+def zona_all():
+    return 'Hello World!'
+
+
+#  get zona info
+@app.route('/zona_one')
+def zona_one():
+    return 'Hello World!'
+
+
+#  update zona info
+@app.route('/zona_edit')
+def zona_edit():
+    return 'Hello World!'
+
+
+#  delete zona
+@app.route('/zona_delete')
+def zona_delete():
     return 'Hello World!'
 
 
