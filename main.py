5370bc52da0fb0c32c2c1ef34c07a0e90a063e88
Alberto Reyes <albertoreyesmartin@gmail.com>
2019-12-03 01:00:21 +0100

    Added create and list treasures endpoints

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
+import models.facade
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route('/')
+def get_treasures():
+    treasures = models.facade.get_treasures()
+    return render_template('treasures.html', treasures=treasures)
 
 
-@app.route("/treasure")  # creates a new treasure
-def treasures():
-    return render_template("treasures.html")
+@app.route('/addTreasure/lat=<latitude>&long=<longitude>&description=<description>')
+def add_treasure(latitude, longitude, description):
+    models.facade.create_treasure(float(latitude), float(longitude), description)
+    treasures = models.facade.get_treasures()
+    return render_template('treasures.html', treasures=treasures)
 
 
 if __name__ == "__main__":  # on running python app.py
     app.run(debug=True)  # run the flask app
-
-
-
