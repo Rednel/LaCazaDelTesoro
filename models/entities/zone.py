fe91a8fd306b431958eef513961130fc0a4580f4
Alberto Reyes <albertoreyesmartin@gmail.com>
2019-12-04 01:46:37 +0100

    Added delete treasure endpoint and create treasure formulary

--- models/entities/zone.py
+++ models/entities/zone.py
@@ -3,9 +3,3 @@ from google.appengine.ext import db
 
 class Zone(db.Model):
     name = db.StringProperty(required=True)
-    latitude = db.StringProperty(required=True)
-    longitude = db.StringProperty(required=True)
-    height = db.StringProperty()
-    width = db.StringProperty()
-    # game = db.ReferenceProperty(Zone, collection_name="zone", required=True)
-
