18ee810ab9a1f7e25cf378583490ca2252cc526e
Alberto Reyes <albertoreyesmartin@gmail.com>
2019-12-12 00:58:07 +0100

    Added treasure functionality of games

--- models/entities/treasure.py
+++ models/entities/treasure.py
@@ -1,7 +1,9 @@
 from google.appengine.ext import db
+from models.entities.game import Game
 
 
 class Treasure(db.Model):
     lat = db.FloatProperty(required=True)
     lon = db.FloatProperty(required=True)
     description = db.StringProperty(default="")
+    game = db.ReferenceProperty(Game, collection_name="treasures")
