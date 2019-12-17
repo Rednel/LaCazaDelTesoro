9b03e66c8f14f081c5c0105b649516538e416355
Tarek Khalfaoui <khalfaoui.t@hotmail.com>
2019-12-17 09:58:40 +0100

    fix blueprint routes for users + zone

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import os
 from views.google import google_bp, login_required
 import requests_toolbelt.adapters.appengine
 from models.entities.user import User
+from models.entities.game import Game
+
 from models.entities.conversation import Conversation
 from models.entities.messages import Message
 
 import time
 from views.google import google_view
-from models.facade import *
+from views.users import users_routes
+from views.zone import zone_routes
 
 # Patch to fix AppEngine
 requests_toolbelt.adapters.appengine.monkeypatch()
@@ -19,6 +22,8 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 app.register_blueprint(google_bp, url_prefix="/")
 app.register_blueprint(google_view, url_prefix="/google")
+app.register_blueprint(users_routes)
+app.register_blueprint(zone_routes)
 
 
 @app.route('/')
@@ -37,129 +42,6 @@ def treasures():
 def test_oauth(user):
     return "You are {email} on Google".format(email=user.email)
 
-
-""" 
-User routes       
-"""
-
-
-# user list
-@app.route('/user_all')
-def user_all():
-    data = get_all_user()
-    return render_template('user/user_all.html', data=data)
-
-
-#  get user info
-@app.route('/user_one/<id>')
-def user_one(id):
-    user_id = int(id)
-    user = get_user_one(user_id)
-    return render_template('user/user_one.html', user=user)
-
-
-#  new user
-@app.route('/user_new', methods=['GET', 'POST'])
-def user_new():
-    if request.method == 'POST':
-        email = request.form.get('email'),
-        name = request.form.get('name'),
-        surname = request.form.get('surname'),
-        picture = request.form.get('picture')
-        insert_user_new(email, name, surname, picture)
-        return redirect('user_all')
-    else:
-        return render_template('user/user_new.html')
-
-
-
-#  update user info
-@app.route('/user_edit/<id>', methods=['GET', 'POST'])
-def user_edit(id):
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        surname = request.form.get('surname')
-        picture = request.form.get('pricture')
-        user_id = int(id)
-        user_update_model(id, name, email, surname, picture)
-        return redirect('/user_all')
-    else:
-        user_id = int(id)
-        user = db.get(db.Key.from_path('User', user_id))
-        return render_template('user/user_edit.html', user=user)
-
-
-#  delete user
-@app.route('/user_delete/<id>')
-def user_delete(id):
-    if id:
-        user_id = int(id)
-        user_delete_model(user_id)
-        return redirect('/user_all')
-
-""" 
-Routes Zona        
-"""
-
-
-# zone list
-@app.route('/zone_all')
-def zone_all():
-    data = get_zone_all()
-    return render_template('zone/zone_all.html', data=data)
-
-
-#  get zone info
-@app.route('/zone_one/<id>')
-def zone_one(id):
-    zone_id = int(id)
-    zone = zone_one_model(zone_id)
-    return render_template('zone/zone_one.html', zone=zone)
-
-
-#  new zone
-@app.route('/zone_new', methods=['GET', 'POST'])
-def zone_new():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        latitude = request.form.get('latitude')
-        longitude = request.form.get('longitude')
-        height = request.form.get('height')
-        width = request.form.get('width')
-        insert_zone_new(name, latitude, longitude, height, width)
-        return redirect('zone_all')
-    else:
-        return render_template('zone/zone_new.html')
-    zone_new()
-
-
-#  update zone info
-@app.route('/zone_edit/<id>', methods=['GET', 'POST'])
-def zone_edit(id):
-        if request.method == 'POST':
-            name = request.form.get('name')
-            latitude = request.form.get('latitude')
-            longitude = request.form.get('longitude')
-            height = request.form.get('height')
-            width = request.form.get('width')
-            zone_id = int(id)
-            zone_edit_model(id, name, latitude, longitude, height, width)
-            return redirect('/zone_all')
-        else:
-            zone_id = int(id)
-            zone = db.get(db.Key.from_path('Zone', zone_id))
-            return render_template('zone/zone_edit.html', zone=zone)
-
-
-#  delete zone
-@app.route('/zone_delete/<id>')
-def zone_delete(id):
-    if id:
-        zone_id = int(id)
-        zone_delete_model(zone_id)
-        return redirect('/zone_all')
-
 """ 
 SEARCH FOR GAME       
 """
@@ -170,8 +52,7 @@ def game_search():
         keyword = str(keyword)
         q = Game.all()
         result = q.filter("game_name =", keyword)
-        return render_template('game_search.html', data=result)
-
+        return render_template('search/game_search.html', data=result)
 
 """ 
 CRUD Mensajes       
